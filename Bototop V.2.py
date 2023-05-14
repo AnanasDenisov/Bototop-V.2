c826186ec515b93419182917a8301a46605af193
AnanasDenisov <131580519+AnanasDenisov@users.noreply.github.com>
2023-05-14 13:47:07 +0500

    Add files via upload

---
+++ Bototop V.2.py
@@ -0,0 +1,81 @@
+import discord
+# import * - это тоже самое, что перечислить все файлы
+from bot_logic import *
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+
+# Когда бот будет готов, он напишет в консоли свое название!
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+# Когда бот будет получать сообщение, он будет отправлять в этот же канал какие-то сообщения!
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+        
+    if message.content.startswith('$orel/reshka'):
+        import random
+
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+
+def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+def cube():
+    cube = random.randint(1, 6)
+    if cube == 1:
+        print(cube)
+
+    if cube == 2:
+        print(cube)
+
+    if cube == 3:
+        print(cube)
+
+    if cube == 4:
+        print(cube)
+
+    if cube == 5:
+        print(cube)
+
+    if cube == 6:
+        print(cube)
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
+        
+    if message.content.startswith('$hello'):
+        await message.channel.send('Привет! Я бот!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send("Я не понимаю такую команду!")
+
+client.run(settings["TOKEN"])
